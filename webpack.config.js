57aa3b77be86022112dc08fe28d586e3679cc106
UninstallGame <zdehnP1992>
2020-08-03 23:13:17 +0800

    Переезд на ts, scss с помощью webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+
+module.exports = [
+    {
+        entry: './script/index.js',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        }
+    },
+    {
+        entry: './style/index.scss',
+        module: {
+            rules: [
+                {
+                    test: /\.(s*)css$/,
+                    use: [
+                        miniCss.loader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.css', '.scss'],
+        },
+        plugins: [
+            new miniCss({
+                filename: 'style.css',
+            }),
+        ]
+    },
+];
